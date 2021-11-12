74aa49fb6ada68aebe0786af18d5edd902276a0d
Roman Abramov <RVAbramov@sberbank.ru>
2021-11-12 19:33:14 +0300

    + задача о биноминальных коэффициентах

--- src/main/java/common/Utils.java
+++ src/main/java/common/Utils.java
@@ -12,4 +12,14 @@ public class Utils {
         System.out.println();
     }
 
+    public static void showArray(long[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
 }
