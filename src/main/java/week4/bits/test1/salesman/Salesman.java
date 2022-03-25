bed36f7ef3bbd5730adba1eb2d58e151fb93fe96
Roman Abramov <RVAbramov@sberbank.ru>
2022-03-25 23:09:54 +0300

    решение задачи коммивояжера

--- src/main/java/week4/bits/test1/salesman/Salesman.java
+++ src/main/java/week4/bits/test1/salesman/Salesman.java
@@ -1,15 +1,25 @@
 package week4.bits.test1.salesman;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Salesman {
 
-    public void solve(int[][] d) {
+    private int[][] d;
+    private Map<String, Integer> cache = new HashMap<>();
+    private static final boolean VERBOSE = false;
+
+    private int minDistance;
+    private List<String> paths = new ArrayList<>();
+
+    public Salesman(int[][] d) {
+        this.d = d;
+    }
+
+    public void solve() {
         int nodes = d.length - 1;
-        Map<String, Integer> cache = new HashMap<>();
+
 
         for (int mask = 0; mask < 1 << nodes; mask++) {
             showMaskInfo(mask, nodes);
@@ -27,13 +37,13 @@ public class Salesman {
                 nodesTo.add(0);
             }
 
-            System.out.println("\t\t" + nodesThrow + " - " + nodesTo);
+            if(VERBOSE) System.out.println("\t\t" + nodesThrow + " - " + nodesTo);
             for (int nodeTo : nodesTo) {
-                System.out.println("\tCost(0, " + nodesThrow + ", " + nodeTo + ") = minimum of");
+                if(VERBOSE) System.out.println("\tCost(0, " + nodesThrow + ", " + nodeTo + ") = minimum of");
                 int min = Integer.MAX_VALUE;
 
                 if (nodesThrow.size() == 0) {
-                    System.out.println("\t\t\t\t\t d[0, " + nodeTo + "]");
+                    if(VERBOSE) System.out.println("\t\t\t\t\t d[0, " + nodeTo + "]");
                     min = Math.min(min, d[0][nodeTo]);
                 }
                 for (int nodeThrow : nodesThrow) {
@@ -42,13 +52,50 @@ public class Salesman {
                     int costSubValue = cache.get(costSubKey);
                     int dist = d[nodeThrow][nodeTo];
                     min = Math.min(min, costSubValue + dist);
-                    System.out.println("\t\t\t\t\t " + costSubKey + "d[" + nodeThrow + ", " + nodeTo + "] = " + costSubValue + " + " + dist + " = " + (costSubValue + dist));
+                    if(VERBOSE) System.out.println("\t\t\t\t\t " + costSubKey + "d[" + nodeThrow + ", " + nodeTo + "] = " + costSubValue + " + " + dist + " = " + (costSubValue + dist));
                 }
 
-                System.out.println("\t\t" + costKey(nodesThrow, nodeTo) + " = " + min);
+                if(VERBOSE) System.out.println("\t\t" + costKey(nodesThrow, nodeTo) + " = " + min);
                 cache.put(costKey(nodesThrow, nodeTo), min);
             }
         }
+
+        List<Integer> subs = IntStream.range(1, nodes + 1).boxed().collect(Collectors.toList());
+        minDistance = cache.get(costKey(subs, 0));
+
+        Integer[] certificate = new Integer[d.length];
+        createCertificate(subs, 0, certificate, 0);
+    }
+
+    private void createCertificate(List<Integer> nodesThrow, Integer nodeTo, Integer[] certificate, int level) {
+        Integer min = cache.get(costKey(nodesThrow, nodeTo));
+        //System.out.println("subs: " + nodesThrow + "\t min: " + min);
+
+        for (int nodeThrow : nodesThrow) {
+            List<Integer> sub = minus(nodesThrow, nodeThrow);
+            String costSubKey = costKey(sub, nodeThrow);
+            int dist = cache.get(costSubKey) + d[nodeThrow][nodeTo];
+
+            if (dist == min) {
+                certificate[level] = nodeThrow;
+
+                createCertificate(sub, nodeThrow, certificate, level+1);
+                if (certificate.length == level + 2) {
+                    String cert = Arrays.stream(certificate).limit(level + 1)
+                            .map(Object::toString)
+                            .collect(Collectors.joining(" "));
+                    paths.add("0 " + cert);
+                }
+            }
+        }
+    }
+
+    public int getMinDistance() {
+        return minDistance;
+    }
+
+    public List<String> getPaths() {
+        return paths;
     }
 
     private List<Integer> minus(List<Integer> list, Integer value) {
@@ -74,9 +121,11 @@ public class Salesman {
     }
 
     private void showMaskInfo(int mask, int nodes) {
-        System.out.println(mask
-                + "\t" + Integer.toBinaryString(mask)
-                + "\t" + maskToBitArray2(mask, nodes));
+        if(VERBOSE) {
+            System.out.println(mask
+                    + "\t" + Integer.toBinaryString(mask)
+                    + "\t" + maskToBitArray2(mask, nodes));
+        }
     }
 
 }
--- src/main/java/week4/bits/test1/salesman/SalesmanTest.java
+++ src/main/java/week4/bits/test1/salesman/SalesmanTest.java
@@ -1,6 +1,12 @@
 package week4.bits.test1.salesman;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
 
 public class SalesmanTest {
 
@@ -20,11 +26,35 @@ public class SalesmanTest {
 
     @Test
     public void test0(){
-        new Salesman().solve(d0);
+        Salesman s = new Salesman(d0);
+        s.solve();
+        Assert.assertEquals(10, s.getMinDistance());
+        Assert.assertEquals("0 1 3 2", s.getPaths().get(0));
     }
 
     @Test
-    public void test2() {
-        new Salesman().solve(d1);
+    public void test1() {
+        Salesman s = new Salesman(d1);
+        s.solve();
+        Assert.assertEquals(29, s.getMinDistance());
+        Assert.assertEquals("0 1 2 3 4", s.getPaths().get(0));
+    }
+
+    @Test
+    public void test2() throws IOException {
+        int[][] d2 = Files.lines(Paths.get("src/main/java/week4/bits/test1/salesman/salesman2.in"))
+                .skip(1)
+                .map(line -> Arrays.stream(line.split("\\s"))
+                        .mapToInt(Integer::valueOf)
+                        .toArray())
+                .toArray(int[][]::new);
+
+        Salesman s = new Salesman(d2);
+        s.solve();
+        System.out.println("minDistance: " + s.getMinDistance());
+        s.getPaths().forEach(System.out::println);
+
+        Assert.assertEquals(111, s.getMinDistance());
+        Assert.assertEquals("0 3 10 8 6 13 14 15 2 7 1 4 9 12 11 5", s.getPaths().get(0));
     }
 }
